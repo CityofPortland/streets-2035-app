a20e64e2ef8380b20024df6a60d1bd1952215c08
Mike McDonald <michael.mcdonald@portlandoregon.gov>
2021-08-04 01:00:06 +0000

    S2WM-2 Fix tailwind in storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,37 @@
+const path = require("path");
+
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials"
-  ]
-}
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: ["@storybook/addon-a11y", "@storybook/addon-links", "@storybook/addon-essentials", {
+    name: '@storybook/addon-postcss',
+    options: {
+      postcssLoaderOptions: {
+        implementation: require('postcss'),
+        postcssOptions: {
+          plugins: [
+            require('tailwindcss'),
+            require('autoprefixer'),
+          ],
+        }
+      },
+    },
+  },],
+  webpackFinal: async (config, { configType }) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname, "../src")
+    }
+
+    config.module.rules.push({
+      resourceQuery: /blockType=i18n/,
+      type: 'javascript/auto',
+      use: [
+        {
+          loader: '@intlify/vue-i18n-loader'
+        }
+      ]
+    })
+
+    return config;
+  }
+};
