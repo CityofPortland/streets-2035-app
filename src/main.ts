788614d76b85d2c76a31373935d6a597da670b6f
Mike McDonald <michael.mcdonald@portlandoregon.gov>
2021-08-04 05:35:29 +0000

    S2WM-2 Add i18n to app config

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,11 @@ import App from './App.vue';
 import './registerServiceWorker';
 import router from './router';
 import store from './store';
+import { createI18n } from 'vue-i18n';
 import 'tailwindcss/tailwind.css';
 
-createApp(App).use(store).use(router).mount('#app');
+const i18n = createI18n({
+  // something vue-i18n options here ...
+});
+
+createApp(App).use(store).use(router).use(i18n).mount('#app');
