2abbd430257236b5f1c00ed32154ffe99c4d8735
Mike McDonald <michael.mcdonald@portlandoregon.gov>
2021-08-05 23:36:10 +0000

    S2WM-6 Add street type routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
 import Disclaimer from '@/pages/Disclaimer.vue';
 import Home from '@/pages/Home.vue';
+import StreetTypes from '@/pages/street-type/StreetType.vue';
+import StreetType1 from '@/pages/street-type/StreetType1.mdx';
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -12,6 +14,24 @@ const routes: Array<RouteRecordRaw> = [
     path: '/disclaimer',
     name: 'Disclaimer',
     component: Disclaimer,
+  },
+  {
+    path: '/street-types/:id?',
+    name: 'StreetTypes',
+    component: StreetTypes,
+    props: (route) => {
+      console.log('In async component');
+
+      const pages = new Map([['street-type-1', StreetType1]]);
+
+      const id = Array.isArray(route.params.id)
+        ? route.params.id[0]
+        : route.params.id;
+
+      return {
+        page: pages.has(id) ? pages.get(id) : Error('No page with that id'),
+      };
+    },
   },
 ];
 
