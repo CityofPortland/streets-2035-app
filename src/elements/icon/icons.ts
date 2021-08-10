1222e515ae4451f0322c1ea28cc0a63937c967ae
Mike McDonald <michael.mcdonald@portlandoregon.gov>
2021-08-09 17:49:11 -0700

    S2WM-7 Add chevron icons from heroicons

--- src/elements/icon/icons.ts
+++ src/elements/icon/icons.ts
@@ -1,9 +1,11 @@
 export default {
   'check-circle': 'M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z',
+  'chevron-down': 'M19 9l-7 7-7-7',
+  'chevron-left': 'M15 19l-7-7 7-7',
+  'chevron-right': 'M9 5l7 7-7 7',
+  'chevron-up': 'M5 15l7-7 7 7',
   'external-link':
     'M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14',
-  'x-circle':
-    'M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z',
   exclamation:
     'M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z',
   flame: [
@@ -12,4 +14,6 @@ export default {
   ],
   minus: 'M20 12H4',
   plus: 'M12 4v16m8-8H4',
+  'x-circle':
+    'M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z',
 } as Record<string, string | Array<string>>;
