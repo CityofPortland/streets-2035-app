eb49c0b7f631314408688422bc4cb1ce2c23e231
Mike McDonald <michael.mcdonald@portlandoregon.gov>
2021-08-10 17:38:45 -0700

    S2WM-7 Add DropdownList component

---
+++ src/components/dropdown/DropdownList.vue
@@ -0,0 +1,39 @@
+<template>
+  <Box
+    as="div"
+    role="menu"
+    tabindex="-1"
+    aria-orientation="vertical"
+    :aria-labelledby="id"
+    :class="{ hidden: !open }"
+  >
+    <div class="p-1 w-full" role="none">
+      <slot></slot>
+    </div>
+  </Box>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+import Box from '@/elements/box/Box';
+
+export default defineComponent({
+  name: 'DropdownList',
+  components: { Box },
+  props: {
+    as: {
+      type: String,
+      default: 'span',
+    },
+    id: {
+      type: String,
+      required: true,
+    },
+    open: {
+      type: Boolean,
+      required: true,
+    },
+  },
+});
+</script>
