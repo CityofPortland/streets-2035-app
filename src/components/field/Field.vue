510c2f2e99ef853b5e2221452b36b5ccc2cd077e
Mike McDonald <michael.mcdonald@portlandoregon.gov>
2021-09-08 20:21:38 +0000

    S2WM-15 Add resuable field component

---
+++ src/components/field/Field.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="grid grid-cols-1 md:grid-cols-3 gap-3 mt-4">
+    <span class="font-semibold text-lg capitalize">
+      <slot name="name">{{ name }}</slot>
+    </span>
+    <span class="md:col-span-2 prose">
+      <slot>{{ value }}</slot>
+    </span>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Field',
+  props: {
+    name: { type: String, required: true },
+    value: { type: String },
+  },
+});
+</script>
