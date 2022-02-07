72c9fc6a602899ef9edeabb3d092af075410feae
Mike McDonald <michael.mcdonald@portlandoregon.gov>
2022-02-07 13:08:01 -0800

    feat: Add FieldList compnent
    Adds FieldList to group fields together
    
    REf: S2WM-36

---
+++ src/components/field/FieldList.vue
@@ -0,0 +1,23 @@
+<template>
+  <Box :as="as" class="grid grid-cols-1">
+    <slot></slot>
+  </Box>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+import Box, { BoxProps } from '@/elements/box/Box';
+
+export default defineComponent({
+  name: 'FieldList',
+  components: { Box },
+  props: {
+    ...BoxProps,
+    as: {
+      type: String,
+      default: 'dl',
+    },
+  },
+});
+</script>
