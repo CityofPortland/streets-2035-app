0fd373b7de0a543614206d9000b7c3bef2d501d9
Mike McDonald <michael.mcdonald@portlandoregon.gov>
2022-02-07 13:09:36 -0800

    refactor: Add useBox composable

--- src/elements/box/Box.stories.ts
+++ src/elements/box/Box.stories.ts
@@ -4,37 +4,38 @@ import Box from './Box';
 
 export const ColorArgs = {
   color: {
+    options: [
+      'blue',
+      'cyan',
+      'gray',
+      'green',
+      'orange',
+      'red',
+      'tangerine',
+      'transparent',
+    ],
     control: {
       type: 'select',
-      options: [
-        'blue',
-        'cyan',
-        'gray',
-        'green',
-        'orange',
-        'red',
-        'tangerine',
-        'transparent',
-      ],
     },
   },
   variant: {
+    options: ['light', 'neutral', 'dark'],
     description:
       'Three different color theme variants for the chosen color theme. All variants are WCAG AA accessible at minimum.',
     control: {
       type: 'select',
-      options: ['light', 'neutral', 'dark'],
     },
   },
 };
 
-const Template: Story = (args, { argTypes }) => ({
+export const Basic: Story = (args, { argTypes }) => ({
   props: Object.keys(argTypes),
   components: { Box },
+  setup() {
+    return { ...args };
+  },
   template: `<Box :as="as" :color="color" :variant="variant" :class="classes">This is a box.</Box>`,
 });
-
-export const Basic = Template.bind({});
 Basic.args = {
   as: 'div',
   classes: '',
--- src/elements/box/Box.ts
+++ src/elements/box/Box.ts
@@ -1,5 +1,7 @@
 import { defineComponent, h } from 'vue';
 
+import { useBox } from '@/composables/use-box';
+
 export type BoxColor =
   | 'blue'
   | 'cyan'
@@ -21,95 +23,31 @@ export interface BoxProps {
   as: string;
 }
 
-export default defineComponent({
-  props: {
-    as: {
-      type: String,
-      default: 'div',
-    },
-    color: {
-      type: String as () => BoxColor,
-      default: 'transparent',
-    },
-    variant: {
-      type: String as () => BoxColorVariant,
-      default: 'neutral',
-    },
+export const BoxProps = {
+  as: {
+    type: String,
+    default: 'div',
   },
-  render() {
-    const classMap = new Map([
-      [
-        'blue',
-        new Map([
-          ['light', ['bg-blue-100', 'text-blue-900']],
-          ['neutral', ['bg-blue-500', 'text-blue-100']],
-          ['dark', ['bg-blue-900', 'text-blue-100']],
-        ]),
-      ],
-      [
-        'cyan',
-        new Map([
-          ['light', ['bg-cyan-100', 'text-cyan-900']],
-          ['neutral', ['bg-cyan-500', 'text-cyan-900']],
-          ['dark', ['bg-cyan-900', 'text-cyan-100']],
-        ]),
-      ],
-      [
-        'gray',
-        new Map([
-          ['light', ['bg-gray-100', 'text-gray-900']],
-          ['neutral', ['bg-gray-500', 'text-gray-100']],
-          ['dark', ['bg-gray-900', 'text-gray-100']],
-        ]),
-      ],
-      [
-        'green',
-        new Map([
-          ['light', ['bg-green-200', 'text-green-900']],
-          ['neutral', ['bg-green-500', 'text-green-100']],
-          ['dark', ['bg-green-900', 'text-green-100']],
-        ]),
-      ],
-      [
-        'orange',
-        new Map([
-          ['light', ['bg-orange-100', 'text-orange-900']],
-          ['neutral', ['bg-orange-500', 'text-orange-900']],
-          ['dark', ['bg-orange-900', 'text-orange-100']],
-        ]),
-      ],
-      [
-        'red',
-        new Map([
-          ['light', ['bg-red-100', 'text-red-900']],
-          ['neutral', ['bg-red-600', 'text-red-100']],
-          ['dark', ['bg-red-900', 'text-red-100']],
-        ]),
-      ],
-      [
-        'tangerine',
-        new Map([
-          ['light', ['bg-tangerine-300', 'text-tangerine-900']],
-          ['neutral', ['bg-tangerine-500', 'text-tangerine-900']],
-          ['dark', ['bg-tangerine-900', 'text-tangerine-100']],
-        ]),
-      ],
-      [
-        'transparent',
-        new Map([
-          ['light', ['bg-transparent']],
-          ['neutral', ['bg-transparent']],
-          ['dark', ['bg-transparent']],
-        ]),
-      ],
-    ]);
+  color: {
+    type: String as () => BoxColor,
+    default: 'transparent',
+  },
+  variant: {
+    type: String as () => BoxColorVariant,
+    default: 'neutral',
+  },
+};
 
-    const classes = classMap.get(this.color)?.get(this.variant);
+export default defineComponent({
+  props: BoxProps,
+  setup(props, { slots }) {
+    const { colorClasses } = useBox(props.color, props.variant);
 
-    return h(
-      this.as,
-      { class: classes },
-      this.$slots.default && this.$slots.default()
-    );
+    return () =>
+      h(
+        props.as,
+        { class: colorClasses.value },
+        slots.default && slots.default()
+      );
   },
 });
