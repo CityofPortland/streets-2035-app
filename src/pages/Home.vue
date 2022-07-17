831c361dc0cbc9b6e5cd71f0efe6c965d082a724
Mike McDonald <michael.mcdonald@portlandoregon.gov>
2022-07-17 04:23:52 +0000

    feat: Add home intro paragraph

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -7,12 +7,18 @@
           variant="dark"
           class="p-8 transform skew-y-1 skew-x-1"
         >
-          <div class="transform -skew-y-1 -skew-x-1">
+          <div class="transform -skew-y-1 -skew-x-1 prose-lg">
             <h1 class="text-4xl font-bold">
               Welcome to
               <abbr title="Portland Bureau of Transportation">PBOT's</abbr>
               Streets 2035 design app!
             </h1>
+            <p>
+              Streets 2035 is a tool for capital and development projects that
+              uses a framework for gathering relevant information at the
+              beginning of a project. It systematically works through tradeoffs,
+              impacts, and outcomes in a consistent manner
+            </p>
             <div
               class="mt-8 flex flex-col md:flex-row items-center justify-center gap-4"
             >
@@ -31,37 +37,6 @@
         </Box>
       </Box>
     </Box>
-    <main class="max-w-7xl mt-8 px-4 mx-auto">
-      <section class="prose">
-        <p>
-          I'm baby vice twee hella cronut bitters. Heirloom semiotics waistcoat
-          gastropub brunch quinoa yr. Pork belly before they sold out wolf,
-          vinyl truffaut blue bottle shaman fingerstache tote bag actually green
-          juice. Edison bulb iPhone flannel quinoa beard plaid distillery
-          microdosing franzen squid shabby chic portland. Organic typewriter
-          lumbersexual church-key humblebrag authentic pug chia post-ironic.
-          Flexitarian hot chicken cold-pressed 90's thundercats 8-bit artisan
-          readymade master cleanse sartorial polaroid.
-        </p>
-        <p>
-          Thundercats snackwave skateboard, hella quinoa green juice venmo
-          before they sold out disrupt vice tote bag. Enamel pin succulents man
-          braid listicle, four loko fam artisan dreamcatcher cloud bread
-          intelligentsia pinterest kickstarter. Yr kombucha selvage twee
-          flannel. Raclette single-origin coffee street art kitsch polaroid
-          XOXO. Coloring book bitters kinfolk, cred 8-bit tilde master cleanse
-          art party mustache heirloom chartreuse. Poke whatever quinoa drinking
-          vinegar, plaid twee polaroid XOXO.
-        </p>
-        <p>
-          Cardigan yuccie viral, occupy asymmetrical lomo gentrify chia roof
-          party. Keffiyeh twee neutra thundercats PBR&B franzen mixtape raw
-          denim. Cronut franzen affogato brunch shaman. Lumbersexual humblebrag
-          narwhal affogato bespoke iceland. Lyft ethical tumblr pinterest marfa
-          viral.
-        </p>
-      </section>
-    </main>
   </article>
 </template>
 
