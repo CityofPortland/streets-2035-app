6d7b4f7b1c79de7a754751237d7c1df959673061
Mike McDonald <michael.mcdonald@portlandoregon.gov>
2022-09-30 14:40:05 -0700

    feat: Add dot scoring components for street types
    ref: S2WM-85
    ref S2WM-60

---
+++ src/components/street-type/Dot.vue
@@ -0,0 +1,6 @@
+<script setup lang="ts">
+import Box from '@/elements/box/Box';
+</script>
+<template>
+  <Box class="bg-current h-4 w-4 rounded-full" />
+</template>
---
+++ src/components/street-type/Score.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import Box from '@/elements/box/Box';
+import Dot from './Dot.vue';
+
+defineProps({
+  label: {
+    type: String,
+  },
+  score: {
+    type: Number,
+  },
+});
+</script>
+
+<template>
+  <Box class="grid grid-cols-2 gap-2 items-center">
+    <label v-if="label">{{ label }}</label>
+    <Box class="flex gap-1 justify-self-end">
+      <Dot v-for="n in score" :key="n" />
+    </Box>
+  </Box>
+</template>
