bb1a6da7625e9129087f01d67778d7eaf9147341
Mike McDonald <michael.mcdonald@portlandoregon.gov>
2022-10-05 10:40:09 -0700

    refactor: Use Card component for street types
    ref: S2WM-60
    ref: S2WM-84

--- src/components/street-type/Score.vue
+++ src/components/street-type/Score.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import Box from '@/elements/box/Box';
-import Dot from './Dot.vue';
 
 defineProps({
   label: {
@@ -16,7 +15,11 @@ defineProps({
   <Box class="flex justify-between gap-2 items-center">
     <label v-if="label">{{ label }}</label>
     <Box class="flex gap-1">
-      <Dot v-for="n in score" :key="n" />
+      <span
+        v-for="n in score"
+        :key="n"
+        class="bg-current h-4 w-4 rounded-full"
+      />
     </Box>
   </Box>
 </template>
