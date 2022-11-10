ea142d46699d22abb3c800f653f353fedbd20eba
Mike McDonald <michael.mcdonald@portlandoregon.gov>
2022-11-09 17:08:22 -0800

    feat: Add ability to move scorecards
    Adds move buttons and emits to scorecards
    Emits handled by scoresheet which wraps option in reactive variables
    
    ref: S2WM-99

--- src/components/cross-section/Scoresheet.vue
+++ src/components/cross-section/Scoresheet.vue
@@ -1,3 +1,46 @@
+<script setup lang="ts">
+import Scorecard from '@/components/cross-section/Scorecard.vue';
+import { CrossSection } from '@/composables/cross-section';
+import { ref, watch } from 'vue';
+import { PrioritySet } from './typings';
+
+const props = defineProps({
+  options: { type: Array as () => Array<CrossSection>, required: true },
+  mainStreet: {
+    type: Boolean,
+    default: false,
+  },
+  priorities: {
+    type: Object as () => PrioritySet,
+    default: () => ({
+      pedestrian: 'higher',
+      bicycle: 'higher',
+      transit: 'higher',
+      freight: 'higher',
+      traffic: 'higher',
+    }),
+  },
+});
+
+// Get a ref copy of the options to manipulate
+const options = ref([...props.options]);
+
+// If we get a new set of options, replace the ref
+watch(
+  () => props.options,
+  (value) => {
+    options.value = value;
+  }
+);
+
+const handleMove = (curr: number, desired: number) => {
+  console.log(`Moving from ${curr} to ${desired}...`);
+  if (desired >= 0 && desired < options.value.length) {
+    options.value.splice(desired, 0, ...options.value.splice(curr, 1));
+  }
+};
+</script>
+
 <template>
   <section
     class="grid grid-flow-col gap-1 overflow-x-auto"
@@ -16,43 +59,15 @@
     <span class="py-2">Traffic</span>
     <span></span>
     <Scorecard
-      v-for="option in options"
-      :key="option.name"
+      v-for="(option, index) in options"
+      :key="`${option.width}:${option.name}`"
       :scores="option"
       :main-street="mainStreet"
       :priorities="priorities"
+      @move-left="handleMove(index, index - 1)"
+      @move-right="handleMove(index, index + 1)"
     >
       <slot :option="option"></slot>
     </Scorecard>
   </section>
 </template>
-
-<script lang="ts">
-import { defineComponent } from 'vue';
-
-import Scorecard from '@/components/cross-section/Scorecard.vue';
-import { CrossSection } from '@/composables/cross-section';
-import { PrioritySet } from './typings';
-
-export default defineComponent({
-  name: 'Scoresheet',
-  components: { Scorecard },
-  props: {
-    options: { type: Array as () => Array<CrossSection>, required: true },
-    mainStreet: {
-      type: Boolean,
-      default: false,
-    },
-    priorities: {
-      type: Object as () => PrioritySet,
-      default: () => ({
-        pedestrian: 'higher',
-        bicycle: 'higher',
-        transit: 'higher',
-        freight: 'higher',
-        traffic: 'higher',
-      }),
-    },
-  },
-});
-</script>
