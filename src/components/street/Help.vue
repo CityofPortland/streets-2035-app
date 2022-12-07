9c5b342a7b1ac2db50c9ec90eb500afe7e749ce7
Mike McDonald <michael.mcdonald@portlandoregon.gov>
2022-12-07 22:49:13 +0000

    fix: Click away in help components

--- src/components/street/Help.vue
+++ src/components/street/Help.vue
@@ -1,15 +1,17 @@
 <template>
-  <div ref="componentRef" class="flex items-center space-x-2 relative">
-    <slot></slot>
-    <button type="button" title="Show more information" @click.stop="toggle">
-      <Icon type="solid" name="question-mark-circle" class="w-4 h-4" />
-    </button>
+  <div ref="componentRef">
+    <section class="flex items-center gap-2">
+      <slot></slot>
+      <button type="button" title="Show more information" @click.stop="toggle">
+        <Icon type="solid" name="question-mark-circle" class="w-4 h-4" />
+      </button>
+    </section>
+    <slot name="help" :help="help" :show="show">
+      <p v-show="show" class="text-xs text-gray-600">
+        {{ help }}
+      </p>
+    </slot>
   </div>
-  <slot name="help" :help="help" :show="show">
-    <p v-show="show" class="text-xs text-gray-600">
-      {{ help }}
-    </p>
-  </slot>
 </template>
 <script lang="ts">
 import { defineComponent, Ref, ref } from 'vue';
