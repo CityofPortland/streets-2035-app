53112f22e4b3e982088d59c0a441fa736587fea7
Mike McDonald <michael.mcdonald@portlandoregon.gov>
2022-12-07 22:39:00 +0000

    feat: Update street type pages
    Consolidate style with other informational pages
    Adds overflow
    Update graph image to include condition scoring again
    
    ref: S2WM-122
    ref: S2WM-123

--- src/components/street-type/Card.vue
+++ src/components/street-type/Card.vue
@@ -11,34 +11,29 @@ defineProps({
 <template>
   <Box
     variant="light"
-    class="p-4 flex flex-col gap-4"
-    style="box-shadow: 10px 10px currentColor"
+    class="m-4 grid grid-cols-1 md:grid-cols-2 gap-4 max-w-prose"
   >
     <Box as="header" class="not-prose grid grid-cols-1 gap-4">
-      <slot></slot>
+      <slot name="header"></slot>
     </Box>
+    <main class="flex flex-col gap-4">
+      <slot></slot>
+      <section class="not-prose flex flex-col gap-2">
+        <p class="text-lg">{{ percentage * 100 }}% of all city streets</p>
+      </section>
 
-    <section class="mt-auto not-prose flex flex-col gap-2">
-      <h3 class="text-lg">Percentage of all city streets</h3>
-      <div class="flex flex-wrap items-center gap-2">
-        <Box
-          as="span"
-          class="bg-current h-4"
-          :style="{ width: `${percentage * 100}%` }"
-        />
-        <p>{{ percentage * 100 }}%</p>
-        <p>(~{{ Math.floor(2090 * percentage) }} miles)</p>
-      </div>
-    </section>
-
-    <section class="not-prose flex flex-col gap-2">
-      <h3 class="text-lg">Typical conditions</h3>
-      <ul class="grid grid-cols-1 gap-2 not-prose">
-        <Score as="li" label="Land Use Intensity" :score="scores[0]" />
-        <Score as="li" label="Roadway Width" :score="scores[1]" />
-        <Score as="li" label="Multimodal Mobility Demand" :score="scores[2]" />
-        <Score as="li" label="Loading/Business Access" :score="scores[3]" />
-      </ul>
-    </section>
+      <section class="not-prose flex flex-col gap-2">
+        <ul class="grid grid-cols-1 gap-2 not-prose">
+          <Score as="li" label="Land Use Intensity" :score="scores[0]" />
+          <Score as="li" label="Roadway Width" :score="scores[1]" />
+          <Score
+            as="li"
+            label="Multimodal Mobility Demand"
+            :score="scores[2]"
+          />
+          <Score as="li" label="Loading/Business Access" :score="scores[3]" />
+        </ul>
+      </section>
+    </main>
   </Box>
 </template>
