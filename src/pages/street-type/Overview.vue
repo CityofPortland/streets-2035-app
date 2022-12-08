8826562187a037a277dc2c11c5f2b94a915867e3
Mike McDonald <michael.mcdonald@portlandoregon.gov>
2022-12-08 05:42:42 +0000

    fix: Prose styling on street type card list

--- src/pages/street-type/Overview.vue
+++ src/pages/street-type/Overview.vue
@@ -48,200 +48,205 @@ const publicPath = process.env.BASE_URL;
       </figure>
 
       <figure class="prose max-w-none">
-        <ul
-          class="not-prose flex flex-row gap-2 flex-nowrap overflow-x-auto snap-x snap-mandatory"
-          role="region"
-          aria-label="Gallery of street types"
-          tabindex="0"
-        >
-          <Card
-            as="li"
-            :scores="[3, 3, 3, 3]"
-            :percentage="0.025"
-            class="min-w-fit snap-center"
+        <div class="not-prose">
+          <ul
+            class="flex flex-row gap-2 flex-nowrap overflow-x-auto snap-x snap-mandatory"
+            role="region"
+            aria-label="Gallery of street types"
+            tabindex="0"
           >
-            <template #header>
-              <Image
-                :src="`${publicPath}img/street-type/CivicMainStreet.webp`"
-                alt="Perspective rendering of a civic main street"
-                title="Perspective rendering of a civic main street"
-              />
-            </template>
-            <h2 class="text-lg">
-              <router-link to="/street-types/civic-main-street">
-                <span class="border-b-2 border-current"
-                  >Civic Main Streets</span
-                >
-              </router-link>
-            </h2>
-            <p>
-              Civic Main Streets are generally Portland's busiest, widest
-              streets that pass through compact, pedestrian-oriented Centers
-              that anchor neighborhoods with retail and businesses, schools and
-              libraries, housing options, employment centers, and gathering
-              places.
-            </p>
-          </Card>
-          <Card
-            as="li"
-            :scores="[2, 3, 3, 2]"
-            :percentage="0.02"
-            class="min-w-fit snap-center"
-          >
-            <template #header>
-              <Image
-                :src="`${publicPath}img/street-type/CivicCorridor.webp`"
-                alt="Perspective rendering of a civic corridor"
-                title="Perspective rendering of a civic corridor"
-              />
-            </template>
-            <h2 class="text-lg">
-              <router-link to="/street-types/civic-corridor">
-                <span class="border-b-2 border-current">Civic Corridors</span>
-              </router-link>
-            </h2>
-            <p>
-              Civic Corridors are generally Portland's busiest, widest, and most
-              prominent streets. As the city grows, Civic Corridors are
-              increasingly lined with multifamily residential and commercial
-              mixed-use developments.
-            </p>
-          </Card>
-          <Card
-            as="li"
-            :scores="[3, 2, 2, 3]"
-            :percentage="0.025"
-            class="min-w-fit snap-center"
-          >
-            <template #header>
-              <Image
-                :src="`${publicPath}img/street-type/NeighborhoodMainStreet.webp`"
-                alt="Perspective rendering of a neighborhood main street"
-                title="Perspective rendering of a neighborhood main street"
-              />
-            </template>
-            <h2 class="text-lg">
-              <router-link to="/street-types/neighborhood-main-street">
-                <span class="border-b-2 border-current"
-                  >Neighborhood Main Street</span
-                >
-              </router-link>
-            </h2>
-            <p>
-              Neighborhood Main Streets are mid-sized streets that pass through
-              smaller neighborhood-serving Centers. These centers support
-              vibrant business districts and residential buildings, bringing
-              amenities, goods, and services within walking distance of
-              neighborhood residents.
-            </p>
-          </Card>
-          <Card
-            as="li"
-            :scores="[2, 2, 2, 2]"
-            :percentage="0.02"
-            class="min-w-fit snap-center"
-          >
-            <template #header>
-              <Image
-                :src="`${publicPath}img/street-type/NeighborhoodCorridor.webp`"
-                alt="Perspective rendering of a neighborhood corridor"
-                title="Perspective rendering of a neighborhood corridor"
-              />
-            </template>
-            <h2 class="text-lg">
-              <router-link to="/street-types/neighborhood-corridor">
-                <span class="border-b-2 border-current"
-                  >Neighborhood Corridors</span
-                >
-              </router-link>
-            </h2>
-            <p>
-              Neighborhood Corridors are generally mid-sized streets that
-              connect surrounding neighborhoods to each other and with other
-              parts of the city. They are zoned to accommodate Portland's growth
-              of residential and commercial buildings in the coming years, but
-              at a smaller scale than Civic Corridors.
-            </p>
-          </Card>
-          <Card
-            as="li"
-            :scores="[1, 2, 2, 1]"
-            :percentage="0.115"
-            class="min-w-fit snap-center"
-          >
-            <template #header>
-              <Image
-                :src="`${publicPath}img/street-type/CommunityCorridor.webp`"
-                alt="Perspective rendering of a community corridor"
-                title="Perspective rendering of a community corridor"
-              />
-            </template>
-            <h2 class="text-lg">
-              <router-link to="/street-types/community-corridor">
-                <span class="border-b-2 border-current"
-                  >Community Corridors</span
-                >
-              </router-link>
-            </h2>
-            <p>
-              Community Corridors are generally mid-sized streets that connect
-              surrounding neighborhoods to each other. Community Corridors are
-              not planned for major growth and feature mostly lower-density
-              residential land uses, some small-scale retail, and neighborhood
-              destinations such as parks and schools.
-            </p>
-          </Card>
-          <Card
-            as="li"
-            :scores="[1, 1, 1, 1]"
-            :percentage="0.77"
-            class="min-w-fit snap-center"
-          >
-            <template #header>
-              <Image
-                :src="`${publicPath}img/street-type/LocalService.webp`"
-                alt="Perspective rendering of a local service street"
-                title="Perspective rendering of a local service street"
-              />
-            </template>
+            <Card
+              as="li"
+              :scores="[3, 3, 3, 3]"
+              :percentage="0.025"
+              class="min-w-fit snap-center"
+            >
+              <template #header>
+                <Image
+                  :src="`${publicPath}img/street-type/CivicMainStreet.webp`"
+                  alt="Perspective rendering of a civic main street"
+                  title="Perspective rendering of a civic main street"
+                />
+              </template>
+              <h2 class="text-lg">
+                <router-link to="/street-types/civic-main-street">
+                  <span class="border-b-2 border-current"
+                    >Civic Main Streets</span
+                  >
+                </router-link>
+              </h2>
+              <p>
+                Civic Main Streets are generally Portland's busiest, widest
+                streets that pass through compact, pedestrian-oriented Centers
+                that anchor neighborhoods with retail and businesses, schools
+                and libraries, housing options, employment centers, and
+                gathering places.
+              </p>
+            </Card>
+            <Card
+              as="li"
+              :scores="[2, 3, 3, 2]"
+              :percentage="0.02"
+              class="min-w-fit snap-center"
+            >
+              <template #header>
+                <Image
+                  :src="`${publicPath}img/street-type/CivicCorridor.webp`"
+                  alt="Perspective rendering of a civic corridor"
+                  title="Perspective rendering of a civic corridor"
+                />
+              </template>
+              <h2 class="text-lg">
+                <router-link to="/street-types/civic-corridor">
+                  <span class="border-b-2 border-current">Civic Corridors</span>
+                </router-link>
+              </h2>
+              <p>
+                Civic Corridors are generally Portland's busiest, widest, and
+                most prominent streets. As the city grows, Civic Corridors are
+                increasingly lined with multifamily residential and commercial
+                mixed-use developments.
+              </p>
+            </Card>
+            <Card
+              as="li"
+              :scores="[3, 2, 2, 3]"
+              :percentage="0.025"
+              class="min-w-fit snap-center"
+            >
+              <template #header>
+                <Image
+                  :src="`${publicPath}img/street-type/NeighborhoodMainStreet.webp`"
+                  alt="Perspective rendering of a neighborhood main street"
+                  title="Perspective rendering of a neighborhood main street"
+                />
+              </template>
+              <h2 class="text-lg">
+                <router-link to="/street-types/neighborhood-main-street">
+                  <span class="border-b-2 border-current"
+                    >Neighborhood Main Street</span
+                  >
+                </router-link>
+              </h2>
+              <p>
+                Neighborhood Main Streets are mid-sized streets that pass
+                through smaller neighborhood-serving Centers. These centers
+                support vibrant business districts and residential buildings,
+                bringing amenities, goods, and services within walking distance
+                of neighborhood residents.
+              </p>
+            </Card>
+            <Card
+              as="li"
+              :scores="[2, 2, 2, 2]"
+              :percentage="0.02"
+              class="min-w-fit snap-center"
+            >
+              <template #header>
+                <Image
+                  :src="`${publicPath}img/street-type/NeighborhoodCorridor.webp`"
+                  alt="Perspective rendering of a neighborhood corridor"
+                  title="Perspective rendering of a neighborhood corridor"
+                />
+              </template>
+              <h2 class="text-lg">
+                <router-link to="/street-types/neighborhood-corridor">
+                  <span class="border-b-2 border-current"
+                    >Neighborhood Corridors</span
+                  >
+                </router-link>
+              </h2>
+              <p>
+                Neighborhood Corridors are generally mid-sized streets that
+                connect surrounding neighborhoods to each other and with other
+                parts of the city. They are zoned to accommodate Portland's
+                growth of residential and commercial buildings in the coming
+                years, but at a smaller scale than Civic Corridors.
+              </p>
+            </Card>
+            <Card
+              as="li"
+              :scores="[1, 2, 2, 1]"
+              :percentage="0.115"
+              class="min-w-fit snap-center"
+            >
+              <template #header>
+                <Image
+                  :src="`${publicPath}img/street-type/CommunityCorridor.webp`"
+                  alt="Perspective rendering of a community corridor"
+                  title="Perspective rendering of a community corridor"
+                />
+              </template>
+              <h2 class="text-lg">
+                <router-link to="/street-types/community-corridor">
+                  <span class="border-b-2 border-current"
+                    >Community Corridors</span
+                  >
+                </router-link>
+              </h2>
+              <p>
+                Community Corridors are generally mid-sized streets that connect
+                surrounding neighborhoods to each other. Community Corridors are
+                not planned for major growth and feature mostly lower-density
+                residential land uses, some small-scale retail, and neighborhood
+                destinations such as parks and schools.
+              </p>
+            </Card>
+            <Card
+              as="li"
+              :scores="[1, 1, 1, 1]"
+              :percentage="0.77"
+              class="min-w-fit snap-center"
+            >
+              <template #header>
+                <Image
+                  :src="`${publicPath}img/street-type/LocalService.webp`"
+                  alt="Perspective rendering of a local service street"
+                  title="Perspective rendering of a local service street"
+                />
+              </template>
 
-            <h2 class="text-lg">
-              <router-link to="/street-types/local-street">
-                <span class="border-b-2 border-current">Local Streets</span>
-              </router-link>
-            </h2>
-            <p>
-              Local streets make up the majority of Portland's streets. These
-              are narrower streets that provide access for people and goods
-              deliveries to residences and destinations like schools and parks.
-            </p>
-          </Card>
-          <Card
-            as="li"
-            :scores="[2, 2, 3, 2]"
-            :percentage="0.02"
-            class="min-w-fit snap-center flex"
-          >
-            <template #header>
-              <Image
-                :src="`${publicPath}img/street-type/Industrial.webp`"
-                alt="Perspective rendering of an industrial road"
-                title="Perspective rendering of an industrial road"
-              />
-            </template>
+              <h2 class="text-lg">
+                <router-link to="/street-types/local-street">
+                  <span class="border-b-2 border-current">Local Streets</span>
+                </router-link>
+              </h2>
+              <p>
+                Local streets make up the majority of Portland's streets. These
+                are narrower streets that provide access for people and goods
+                deliveries to residences and destinations like schools and
+                parks.
+              </p>
+            </Card>
+            <Card
+              as="li"
+              :scores="[2, 2, 3, 2]"
+              :percentage="0.02"
+              class="min-w-fit snap-center flex"
+            >
+              <template #header>
+                <Image
+                  :src="`${publicPath}img/street-type/Industrial.webp`"
+                  alt="Perspective rendering of an industrial road"
+                  title="Perspective rendering of an industrial road"
+                />
+              </template>
 
-            <h2 class="text-lg">
-              <router-link to="/street-types/industrial-road">
-                <span class="border-b-2 border-current">Industrial Roads</span>
-              </router-link>
-            </h2>
-            <p>
-              Industrial roads serve industrial land uses and provide freight
-              connections to regional transportation facilities such as
-              interstate and state highways.
-            </p>
-          </Card>
-        </ul>
+              <h2 class="text-lg">
+                <router-link to="/street-types/industrial-road">
+                  <span class="border-b-2 border-current"
+                    >Industrial Roads</span
+                  >
+                </router-link>
+              </h2>
+              <p>
+                Industrial roads serve industrial land uses and provide freight
+                connections to regional transportation facilities such as
+                interstate and state highways.
+              </p>
+            </Card>
+          </ul>
+        </div>
         <figcaption>
           <p>
             The street types identify general design priorities and
